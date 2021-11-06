e4fcf13684cd2a1cd8211f18aa6ed47b4f4b02fb
face0u0 <me@face0u0.net>
2021-11-07 00:03:02 +0900

    keep

--- returnmap.py
+++ returnmap.py
@@ -2,38 +2,40 @@ from pathlib import Path
 from share.blocks import Constructor, LabelOption, ReturnmapBlock
 from share.parse_csv import CsvSource, read_csv
 
-FILEPATH = Path("result.csv")
-DATALEN = 3
+FILEPATH = Path("out/chebyt2-5.csv")
+DATALEN = 4
 LABELS = [ LabelOption(label=f"T{i+2}") for i in range(DATALEN) ]
 NO_LABELS = [ LabelOption() for _ in range(DATALEN) ]
+TITLE = "chebyshev"
 
 BLOCKS = [
 	ReturnmapBlock(
 		A=read_csv(CsvSource(
 			path=FILEPATH,
 			start=1,
-			end=3
+			end=4
 		)), 
 		labels=LABELS,
 		title="source"
 	),ReturnmapBlock(
 		A=read_csv(CsvSource(
 			path=FILEPATH,
-			start=5,
-			end=7
+			start=6,
+			end=9
 		)), 
 		labels=NO_LABELS,
 		title="mixed"
 	),ReturnmapBlock(
 		A=read_csv(CsvSource(
 			path=FILEPATH,
-			start=9,
-			end=11
+			start=11,
+			end=14
 		)), 
 		labels=NO_LABELS,
 		title="reconstruct"
 	)
 ]
 
-C = Constructor(BLOCKS, title="chebyt", align="h")
-C.save(Path("./graph.png"))
+C = Constructor(BLOCKS, title=TITLE, align="h", w=12)
+SAVEFILE = FILEPATH.parent / (FILEPATH.stem + ".png")
+C.save(SAVEFILE)
--- share/blocks.py
+++ share/blocks.py
@@ -40,11 +40,15 @@ class ReturnmapBlock:
     A: np.ndarray
     labels: List[LabelOption]
     title: str = None
+    index_txt = False
 
     def plot(self, ax: plt.Axes):
         ax.set_title(self.title)
         for j in range(self.A.shape[0]): # 各系列
             ax.scatter(self.A[j][0:-1], self.A[j][1:], s=6, alpha=0.5, label=self.labels[j].label)
+            if self.index_txt:
+                for i in range(self.A.shape[1]-1):
+                    ax.text(self.A[j,i], self.A[j,i+1], str(i))
         if any(map(lambda e : e.label != None, self.labels)):
             ax.legend(loc='upper right')
 	
