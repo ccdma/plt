e53b346938f6178f09af1c26d91e9f31be061335
face0u0 <me@face0u0.net>
2021-11-07 00:21:02 +0900

    push

--- plot.py
+++ plot.py
@@ -3,34 +3,34 @@ from share.blocks import Constructor, LabelOption, PlotBlock, ReturnmapBlock
 from share.parse_csv import CsvSource, read_csv
 from static import *
 
-FILEPATH = BASEDIR / Path("result.csv")
+FILEPATH = BASEDIR / Path("sin1-4.csv")
 DATALEN = 4
-LABELS = [ LabelOption(label=f"sin{i+2}") for i in range(DATALEN) ]
+LABELS = [ LabelOption(label=f"sin[x/(10*{i+1})]") for i in range(DATALEN) ]
 NO_LABELS = [ LabelOption() for _ in range(DATALEN) ]
-TITLE = "chebyshev"
+TITLE = "sin"
 
 BLOCKS = [
 	PlotBlock(
 		A=read_csv(CsvSource(
 			path=FILEPATH,
 			start=1,
-			end=3
+			end=4
 		)), 
 		labels=LABELS,
 		title="source"
 	),PlotBlock(
 		A=read_csv(CsvSource(
 			path=FILEPATH,
-			start=5,
-			end=7
+			start=6,
+			end=9
 		)), 
 		labels=NO_LABELS,
 		title="mixed"
 	),PlotBlock(
 		A=read_csv(CsvSource(
 			path=FILEPATH,
-			start=9,
-			end=11
+			start=11,
+			end=14
 		)), 
 		labels=NO_LABELS,
 		title="reconstruct"
