875b08684ad3523990acd75e188bab76f214ae61
face0u0 <me@face0u0.net>
2021-11-24 16:31:42 +0900

    gen

--- const-primitive-root.py
+++ const-primitive-root.py
@@ -1,29 +1,26 @@
-from matplotlib.pyplot import savefig
+from matplotlib.pyplot import plot
 from share import *
 from static import *
 from pica.ica import *
 
 p = 173
-q = 59
+q = 46
 k = 1
 
-fig, axes = subplots(nrows=2,figsize=(5,10))
+fig, axes = subplots(nrows=2, ncols=2, figsize=(10,10))
 
-samplings = const_powerd_samples(q, -2*np.pi*k/p, p)
-ax = axes[0]
-ax.scatter(samplings.real, samplings.imag, s=2)
-ax.plot(samplings.real, samplings.imag, lw=0.5)
-ax.scatter(samplings.real[0], samplings.imag[0])
-ax.scatter(samplings.real[-1], samplings.imag[-1])
-ax.set_title(f"p={p},q={q} primitive root code (send)")
+def plot_primitive(p, q, ax):
+	samplings = primitive_root_code(p, q)
+	ax.scatter(samplings.real, samplings.imag, s=2)
+	ax.plot(samplings.real, samplings.imag, lw=0.5)
+	# ax.scatter(samplings.real[0], samplings.imag[0])
+	# ax.scatter(samplings.real[-1], samplings.imag[-1])
+	ax.set_title(f"p={p},q={q} primitive root code (generate)")
 
-samplings = primitive_root_code(p, q)
-ax = axes[1]
-ax.scatter(samplings.real, samplings.imag, s=2)
-ax.plot(samplings.real, samplings.imag, lw=0.5)
-ax.scatter(samplings.real[0], samplings.imag[0])
-ax.scatter(samplings.real[-1], samplings.imag[-1])
-ax.set_title(f"p={p},q={q} primitive root code (send)")
+plot_primitive(173, 66, axes[0])
+plot_primitive(173, 45, axes[1])
+plot_primitive(173, 46, axes[2])
+plot_primitive(173, 59, axes[3])
 
 fig.tight_layout()
 plt.show()
