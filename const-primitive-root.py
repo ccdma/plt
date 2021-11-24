8a36a585e28dd138309c26e452f4e57c654a602b
face0u0 <me@face0u0.net>
2021-11-24 17:26:19 +0900

    add func

--- const-primitive-root.py
+++ const-primitive-root.py
@@ -3,8 +3,8 @@ from share import *
 from static import *
 from pica.ica import *
 
-p = 173
-q = 46
+p = 1024
+q = 2
 k = 1
 
 fig, axes = subplots(nrows=2, ncols=2, figsize=(10,10))
@@ -15,12 +15,12 @@ def plot_primitive(p, q, ax):
 	ax.plot(samplings.real, samplings.imag, lw=0.5)
 	# ax.scatter(samplings.real[0], samplings.imag[0])
 	# ax.scatter(samplings.real[-1], samplings.imag[-1])
-	ax.set_title(f"p={p},q={q} primitive root code (generate)")
+	ax.set_title(f"p={p},q={q} primitive root code (generated)")
 
 plot_primitive(173, 66, axes[0])
 plot_primitive(173, 45, axes[1])
 plot_primitive(173, 46, axes[2])
-plot_primitive(173, 59, axes[3])
+plot_primitive(1021, 10, axes[3])
 
 fig.tight_layout()
 plt.show()
--- iq.py
+++ iq.py
@@ -1,30 +1,24 @@
 from pathlib import Path
 import sys
 from numpy import sqrt
-from share.parse_csv import CsvSource, read_csv
+from share import *
 from static import *
 from pica.ica import *
 
-FILEPATH = BASEDIR / Path("const2.csv")
+FILEPATH = BASEDIR / Path("write.csv")
 
 DATA = read_csv(CsvSource(FILEPATH, start=1, end=2))
 CDATA = np.array(DATA[0]) + np.array(DATA[1])*1j
 
 CDATA = CDATA/np.abs(CDATA)
 
-# fig = plt.figure(figsize=(5,5))
-fig, axes = plt.subplots(4,4,figsize=(10,10))
-s=int(sys.argv[1]); l=30
-axes = sum(axes.tolist(), [])
+fig, axes = subplots(1,1,figsize=(5,5))
+ax = axes[0]
 
-for i,ax in enumerate(axes):
-	
-	sliced = CDATA[s+i*l:s+l+i*l]
-
-	#### IQ
-	ax.scatter(sliced.real, sliced.imag, s=2)
-	ax.plot(sliced.real, sliced.imag, lw=0.5)
-	ax.set_title(f"{s+i*l}-{s+l+i*l}")
+#### IQ
+ax.scatter(CDATA.real, CDATA.imag, s=2)
+ax.plot(CDATA.real, CDATA.imag, lw=0.5)
+ax.set_title(f"p={173},q={45} primitive root code (received)")
 
 SAVEFILE = FILEPATH.parent / (FILEPATH.stem + ".png")
 plt.gca().set_aspect('equal','datalim')
