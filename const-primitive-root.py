0ad8ca23f3e1eaf8391a700ee2fe68e89dd35381
face0u0 <me@face0u0.net>
2021-11-24 17:55:18 +0900

    push

--- const-primitive-root.py
+++ const-primitive-root.py
@@ -7,20 +7,26 @@ p = 1024
 q = 2
 k = 1
 
-fig, axes = subplots(nrows=2, ncols=2, figsize=(10,10))
+nrows = 2
+ncols = 3
+fig, axes = subplots(nrows=nrows, ncols=ncols, figsize=(ncols*5,nrows*5))
+fig.suptitle(f"primitive root code (generated)", fontsize=12)
+fig.gca().set_aspect('equal','datalim')
 
 def plot_primitive(p, q, ax):
 	samplings = primitive_root_code(p, q)
-	ax.scatter(samplings.real, samplings.imag, s=2)
-	ax.plot(samplings.real, samplings.imag, lw=0.5)
+	ax.scatter(samplings.real, samplings.imag, s=1)
+	ax.plot(samplings.real, samplings.imag, lw=0.2)
 	# ax.scatter(samplings.real[0], samplings.imag[0])
 	# ax.scatter(samplings.real[-1], samplings.imag[-1])
-	ax.set_title(f"p={p},q={q} primitive root code (generated)")
+	ax.set_title(f"p={p},q={q}")
 
-plot_primitive(173, 66, axes[0])
+plot_primitive(173, 59, axes[0])
 plot_primitive(173, 45, axes[1])
 plot_primitive(173, 46, axes[2])
-plot_primitive(1019, 2, axes[3])
+plot_primitive(173, 2, axes[3])
+plot_primitive(389, 2, axes[4])
+plot_primitive(1019, 2, axes[5])
 
 fig.tight_layout()
 plt.show()
