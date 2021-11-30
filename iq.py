46d618a481a31db57d75ef7afac3f12d5aab1e3f
face0u0 <me@face0u0.net>
2021-11-30 14:25:12 +0900

    push

--- iq.py
+++ iq.py
@@ -5,28 +5,40 @@ from share import *
 from static import *
 from pica.ica import *
 
-FILEPATH = BASEDIR / Path("primitive-1019-2.csv")
+np.random.seed(1)
+
+FILEPATH = BASEDIR / Path("circle.csv")
 
 DATA = read_csv(CsvSource(FILEPATH, start=1, end=2))
 CDATA = np.array(DATA[0]) + np.array(DATA[1])*1j
+CDATA = np.array(np.exp(np.linspace(0, 2*np.pi, 1024)*1j))
+# CDATA = np.tile(primitive_root_code(1019, 2), 20)
+# CDATA = CDATA * np.exp(np.random.normal(0, 0.1, CDATA.shape[0])*1j)
 
+# CDATA = CDATA - np.mean(CDATA)
 CDATA = CDATA/np.abs(CDATA)
+# CDATA = np.random.permutation(CDATA)
 
-fig, axes = subplots(4,4,figsize=(10,10))
+ncols = 1
+nrows = 1
+fig, axes = subplots(ncols=ncols,nrows=nrows,figsize=(5,5))
+ax = axes[0]
+ax.scatter(CDATA.real, CDATA.imag, s=1)
+ax.plot(CDATA.real, CDATA.imag, lw=0.2)
 
-for i in range(16):
-	ax = axes[i]
-	step = 30
-	start = 1000 + i*step
-	end = start + step
-	C = CDATA[start:end]
-	#### IQ
-	ax.scatter(C.real, C.imag, s=2)
-	ax.plot(C.real, C.imag, lw=0.5)
-	ax.set_title(f"{start}-{end}")
+# for i in range(ncols*nrows):
+# 	ax = axes[i]
+# 	step = 120
+# 	start = 1000 + i*step
+# 	end = start + step
+# 	C = CDATA[start:end]
+# 	#### IQ
+	# ax.scatter(C.real, C.imag, s=1)
+	# ax.plot(C.real, C.imag, lw=0.2)
+# 	ax.set_title(f"{start}-{end}")
 
 SAVEFILE = FILEPATH.parent / (FILEPATH.stem + ".png")
 plt.gca().set_aspect('equal','datalim')
-fig.suptitle(f"p={FILEPATH.stem.split('-')[1]},q={FILEPATH.stem.split('-')[2]} primitive root code (received)", fontsize=16)
+# fig.suptitle(f"{' '.join(FILEPATH.stem.split('-'))}", fontsize=16)
 plt.tight_layout()
-fig.savefig(SAVEFILE)
+fig.savefig(FILEPATH.parent / ("circle" + ".png"))
