879dc2bd48a37f5715cd696e2b00cd8aeace5bcb
face0u0 <me@face0u0.net>
2021-12-27 17:44:05 +0900

    push

--- over_0.01_ber.py
+++ over_0.01_ber.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-csv = pd.read_csv("./out/r.csv")
+csv = pd.read_csv("./out/r.csv", delimiter="\t")
 out = []
 
-smpls = list(set(csv["samplings"].tolist())).sort()
+smpls = list(set(csv["samplings"].tolist()))
+smpls.sort()
 for smpl in smpls:
 	targets = csv.query(f"samplings == {smpl}")
 	if targets.size < 2:
 		raise Exception("size < 2")
-	out.extend(targets[-2:-1].to_dict(orient='records'))
+	targets = targets.sort_values("signals")
+	for i, target in targets[::-1].iterrows(): # reverse order
+		if target["ber"] < 0.001:
+			out.append(target.to_dict())
+			break
 
 csvo = pd.DataFrame(
 	data=out
