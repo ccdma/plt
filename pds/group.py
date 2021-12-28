359829ec45b195196caac92ea21db88fb08aa2c5
face0u0 <me@face0u0.net>
2021-12-28 12:18:42 +0900

    push

---
+++ pds/group.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+csv = pd.read_csv("pds/r.csv", delimiter="\t")
+merged = {}
+
+key_str = "signals"
+group_str = "samplings"
+groups = list(set(csv[group_str].tolist()))
+groups.sort()
+for group in groups:
+	targets = csv.query(f"{group_str} == {group}")
+	targets = targets.sort_values(key_str)
+	for i, target in targets.iterrows():
+		row = {key_str: target[key_str]}
+		if target[key_str] in merged:
+			row = merged[target[key_str]]
+		row[f"{group_str}:{group}"] = target[group_str]
+		merged[target[key_str]] = row
+
+csvo = pd.DataFrame(
+	data=merged.values()
+)
+csvo.to_csv("./rw.csv")
