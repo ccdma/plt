a2a92efc7366c212bf7c08d391810b1a5d0f7a1b
face0u0 <me@face0u0.net>
2022-01-23 18:47:39 +0900

    push

---
+++ pds_plot.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import multivariate_normal
+
+df = pd.read_csv("out/r.csv", delimiter="\t")
+
+signals = df.loc[:, "signals"]
+samplings = df.loc[:, "samplings"]
+ber = df.loc[:, "ber"]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter3D(signals, samplings, ber)
+ax.set_title("Surface Plot")
+ax.set_zscale('log')
+fig.show()
+plt.show()
