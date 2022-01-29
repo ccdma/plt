f1b946f6fdbc164ee0525f3d378c3ae938211376
face0u0 <me@face0u0.net>
2022-01-29 15:32:23 +0900

    push

--- ccdma.py
+++ ccdma.py
@@ -14,7 +14,7 @@ def test(signal: int, N: int):
 	bitlen = 1000
 	length = N * bitlen
 	norm_scale = 0.01
-	S = np.array([weyl_samples(i/signal, i/signal, length) for i in range(signal)])
+	S = np.array([weyl_samples(i/signal, 1/(2*N), length) for i in range(signal)])
 	# S = np.array([const_powerd_samples(2, np.random.random(), length) for i in range(signal)])
 	BITS = np.array([[ np.sign(np.random.randint(0, 2)-0.5) for i in range(bitlen) ] for _ in range(signal)])
 	B = np.repeat(BITS, N, axis=1)
@@ -29,12 +29,12 @@ def test(signal: int, N: int):
 	ber = np.abs(BITS_R - BITS).mean()/2
 	return ber
 
-K = np.arange(2, 20)
+K_List = np.arange(2, 20)
 N = 5
 bers = []
-for signal in K:
-	bers.append(test(signal, N))
-plt.plot(K, bers, label="simulation")
+for K in K_List: # number of users
+	bers.append(test(K, N))
+plt.plot(K_List, bers, label="simulation")
 # bers = cdma_ber(N, K)
 # plt.plot(K, bers, label="formula")
 plt.yscale("log")
