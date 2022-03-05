419cf8e55fe39962860d785078068ea432ac4aec
face0u0 <me@face0u0.net>
2022-03-05 14:50:10 +0900

    push

---
+++ ber.py
@@ -0,0 +1,19 @@
+from math import erfc
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as ss
+
+def cdma_ber(N, sigma, K: np.array):
+    return 1/2 * ss.erfc(N/np.sqrt(2*((K-1)*N + sigma**2)))
+
+N = 1000
+sigma = 0.5 #0.01 # 36[db]
+K = np.arange(9, 30)
+BER = cdma_ber(N, sigma, K)
+
+plt.yscale("log")
+plt.plot(K, BER)
+plt.xlabel("K: number of users")
+plt.ylabel("BER")
+plt.show()
+
